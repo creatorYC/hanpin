7f76dc3ee9a5985ef5355c07399462426df8712f
creatorYC <1522120424@qq.com>
2018-06-05 17:27:04 +0800

    完成主要逻辑编写

--- scripts/background.js
+++ scripts/background.js
@@ -5,12 +5,17 @@ class Background{
         chrome.runtime.onMessage.addListener(
             function(message, sender, sendResponse){
                 if(message.method === "getSelection"){
-                    // console.log("background: "+message.data);
+                    // console.log("background: " + message.data);
                     var data = message.data;
-                    // var pinyin = require("pinyin");
-                    // var resp = pinyin(data);
-                    // console.log("background: "+resp);
-                    sendResponse({farewell: data});
+                    var pinyin = require("pinyin");
+                    var result = pinyin(data);
+                    console.log("background: " + result);
+                    let retData = {
+                        method: "return_pinyin",
+                        data: result
+                    }
+                    console.log("background: " + retData);
+                    sendResponse({retData: retData});
                 }
             }
         );
--- scripts/selection.js
+++ scripts/selection.js
@@ -1,7 +1,7 @@
 
 class Select{
 
-    //获取选中内容
+    // 获取选中内容
     getSelectedText(){
         if(window.getSelection){
             return window.getSelection().toString();
@@ -14,7 +14,7 @@ class Select{
         }
     }
 
-    //组装需要发送的消息
+    // 组装需要发送的消息
     getSelectedMessage(){
         const text = this.getSelectedText();
         let message = {
@@ -24,25 +24,36 @@ class Select{
         return message;
     }
 
-    //发送消息
+    // 发送消息
     setSelectionMessage(message){
         chrome.runtime.sendMessage(message, function(response){
-            console.log("selection: "+response.farewell);
+            var retData = response.retData;
+            // console.log("selection: " + retData);
+            if(retData.method === "return_pinyin"){
+                console.log("selection: " + retData.data);
+                var panel = document.getElementById("pop_box");
+                if(panel){
+                    panel.innerText = retData.data;
+                }else{
+                    panel.innerText = "Hello";
+                }
+            } 
         });
     }
 
+    // 生成图标
     getImage(){
         var img = new Image();
         var src = chrome.extension.getURL('images/icon.png');
         img.src = src;
-        img.style.display ='none';
+        img.style.display = 'none';
         img.style.position = "absolute";
         img.style.cursor = 'pointer';
         document.body.appendChild(img);
         return img;
     }
 
-    //鼠标弹起事件
+    // 鼠标弹起事件
     listenMouseUp(img){
         var that = this;
         document.onmouseup = function(e){
@@ -52,8 +63,8 @@ class Select{
             var left = (e.clientX - 30 < 0) ? e.clientX + 15 : e.clientX - 30;
             var top = (e.clientY - 30 < 0) ? e.clientY + sh + 15 : e.clientY + sh - 30;
             
-            setTimeout(function(){  //异步调用，避免onmouseup与onclick事件冲突
-                //有选中的内容
+            setTimeout(function(){  // 异步调用，避免onmouseup与onclick事件冲突
+                // 有选中的内容
                 if(that.getSelectedText().length > 0){
                     img.style.display = "inline";
                     img.style.left = left + "px";
@@ -62,12 +73,24 @@ class Select{
             }, 200);
         };
 
-        //点击图片
+        // 点击图片
         img.onclick = function(e){
             var message = that.getSelectedMessage();
-            that.setSelectionMessage(message);
+            
+
+            setTimeout(function(){
+                that.setSelectionMessage(message);
+            }, 200);
+
+            var e = e || window.event;
+            var sh = window.pageYOffset || document.documentElement.scrollTop || 
+                        document.body.scrollTop || 0;            
+            setTimeout(function(){  // 创建panel
+                that.createPopPanel(e.clientX+30, e.clientY+sh);
+            }, 100);
         };
-        //鼠标松开会触发document的mouseup事件/冒泡
+
+        // 鼠标松开会触发document的mouseup事件/冒泡
         img.onmouseup = function(e){
             var e = e || window.event;
             e.cancelBubble = true;
@@ -75,11 +98,25 @@ class Select{
         return this;
     }
 
+    // 监听页面点击事件
     listenDocumentClick(img){
         document.onclick = function(e){
             img.style.display = "none";
+            var panel = document.getElementById("pop_box");
+            if(panel){  // 移除panel
+                document.body.removeChild(panel);
+            }
         };
         return this;
+    }
+
+    createPopPanel(left, top) {
+        var panel = document.createElement('div');
+        panel.id = 'pop_box';
+        // panel.innerText = "Hello World!";
+        panel.style.left = left + "px";
+        panel.style.top = top + "px";
+        document.body.appendChild(panel);
     }
 }
 
